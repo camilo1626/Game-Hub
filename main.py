a44f65f36f7471e8ebf70ccf9328a140696b539d
juancho <juangarciacastri227@gmail.com>
2026-09-15 19:15:46 -0500

    Agrega clases Estrategia, Shooter y Educativo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from videojuegos.aventura import JuegoAventura
 from videojuegos.competitivo import JuegoCompetitivo
+from videojuegos.terror import JuegoTerror
 from videojuegos.gestor_videojuegos import GestorVideojuegos
+from videojuegos.estrategia import JuegoEstrategia
+from videojuegos.educativo import JuegoEducativo
+from videojuegos.shooter import JuegoDisparos
 
 
 gestor = GestorVideojuegos()
@@ -11,18 +15,41 @@ def registrar_videojuego():
 
     nombre = input("Digite el nombre del videojuego: ")
     genero = input("Digite el género del videojuego: ")
+    plataforma = input("ingrese la plataforma: ")
+    año_lanzamiento = input("ingrese el año lanzamiento: ")
+    precio = input("ingrese el precio: ")
+    desarrollador = input("ingrese el desarrollador: ")
+
 
     print("\nSeleccione el tipo de videojuego:")
     print("1. Aventura")
     print("2. Competitivo")
+    print("3. Terror")
+    print("4. Estrategia")
+    print("5. Educativo")
+    print("6. Shooter")
+
+
 
     tipo = input("Digite una opción: ")
 
     if tipo == "1":
-        juego = JuegoAventura(nombre, genero)
+        juego = JuegoAventura(nombre, genero, plataforma, año_lanzamiento, precio, desarrollador)
 
     elif tipo == "2":
-        juego = JuegoCompetitivo(nombre, genero)
+        juego = JuegoCompetitivo(nombre, genero, plataforma, año_lanzamiento, precio, desarrollador)
+
+    elif tipo == "3":
+        juego = JuegoTerror(nombre,genero, plataforma, año_lanzamiento, precio, desarrollador)
+
+    elif tipo == "4":
+        juego = JuegoEstrategia(nombre,genero,plataforma,año_lanzamiento,precio,desarrollador)
+
+    elif tipo == "5":
+        juego = JuegoEducativo(nombre,genero,plataforma,año_lanzamiento,precio,desarrollador)
+
+    elif tipo == "6":
+        juego = JuegoDisparos(nombre,genero,plataforma,año_lanzamiento,precio,desarrollador)
 
     else:
         print("Opción no válida.")
@@ -54,5 +81,3 @@ while True:
 
     else:
         print("\nOpción no válida.")
-        
-print("Modificacion realizada por Camilo")
